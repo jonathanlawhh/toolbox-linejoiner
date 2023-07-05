dbba50183c86ed57e522694aba87efee4f0bfb08
JOLAW9 <jonathanhuihao.law@inter.ikea.com>
2023-07-05 11:36:10 +0800

    Remove tab configuration

--- pages/index.vue
+++ pages/index.vue
@@ -10,31 +10,8 @@
     <v-row>
       <v-col cols="12">
         <v-card variant="flat">
-          <v-row>
-            <v-col cols="12" md="8" lg="9">
-              <v-tabs v-model="tab">
-                <v-tab :value="i" v-for="i in tabs" :key="i">{{ i }}</v-tab>
-              </v-tabs>
-            </v-col>
-            <v-col cols="12" md="4" lg="3" class="text-right">
-              <v-btn size="large"
-                     @click="tabs.push('t' + Math.floor(Math.random(0, 1) * 3000).toString()); tab = tabs[tabs.length - 1]"
-                     class="ma-2"
-                     variant="outlined">+
-              </v-btn>
-              <v-btn size="large"
-                     @click="tabs.length > 1 && (tab = tabs.length > 1 ? tabs[tabs.indexOf(tab) - 1] : tabs[0]); tabs.splice(tabs.indexOf(tab) + 1, 1)"
-                     class="ma-2" variant="outlined">-
-              </v-btn>
-            </v-col>
-          </v-row>
-
           <v-card-text style="min-height: 240px">
-            <v-window v-model="tab">
-              <v-window-item :value="i" v-for="(i, idx) in tabs" :key="idx">
-                <Inputs></Inputs>
-              </v-window-item>
-            </v-window>
+            <Inputs></Inputs>
           </v-card-text>
         </v-card>
       </v-col>
@@ -45,11 +22,6 @@
 
 <script>
 export default {
-  data: () => ({
-    tabs: ['Default'],
-    tab: '',
-    active_tab_name: 1
-  }),
 }
 </script>
 
