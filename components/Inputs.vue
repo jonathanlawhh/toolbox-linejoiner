7928652c966f3f4573dfc45eec8bcb7d452431a8
JOLAW9 <jonathanhuihao.law@inter.ikea.com>
2023-07-05 11:32:42 +0800

    Added substring function

--- components/Inputs.vue
+++ components/Inputs.vue
@@ -2,7 +2,8 @@
   <div>
     <v-row>
       <v-col cols="12">
-        <p>{{ total_lines }} lines | {{ total_dup }} duplicate/s <span v-if="remove_duplicates && total_dup > 0"> removed</span></p>
+        <p>{{ total_lines }} lines | {{ total_dup }} duplicate/s <span v-if="remove_duplicates && total_dup > 0"> removed</span>
+        </p>
       </v-col>
       <v-col cols="12" md="4" lg="5">
         <v-textarea rows="12" no-resize variant="outlined" label="Input" v-model='input_data'
@@ -39,7 +40,8 @@
                   <h3>For each item configuration</h3>
                 </v-col>
                 <v-col cols="4" md="4" lg="2">
-                  <v-text-field label="Delimiter" variant="outlined" v-model="delim" @change="joinText" color="primary"></v-text-field>
+                  <v-text-field label="Delimiter" variant="outlined" v-model="delim" @change="joinText"
+                                color="primary"></v-text-field>
                 </v-col>
                 <v-col cols="4" md="2" lg="1">
                   <v-text-field label="Start with" variant="outlined" v-model="each_delim_start"
@@ -47,6 +49,17 @@
                 </v-col>
                 <v-col cols="4" md="2" lg="1">
                   <v-text-field label="End with" variant="outlined" v-model="each_delim_end"
+                                @change="joinText" color="secondary"></v-text-field>
+                </v-col>
+
+                <v-col cols="12"></v-col>
+
+                <v-col cols="6" md="3" lg="2">
+                  <v-text-field label="Substring start position" variant="outlined" v-model="substr_start"
+                                @change="joinText" color="primary"></v-text-field>
+                </v-col>
+                <v-col cols="6" md="3" lg="2">
+                  <v-text-field label="Substring end position" variant="outlined" v-model="substr_end"
                                 @change="joinText" color="secondary"></v-text-field>
                 </v-col>
               </v-row>
@@ -65,11 +78,13 @@
                 </v-col>
 
                 <v-col cols="6" md="3" lg="2">
-                  <v-checkbox label="Sort output" v-model="sort_output" @change="joinText" color="secondary"></v-checkbox>
+                  <v-checkbox label="Sort output" v-model="sort_output" @change="joinText"
+                              color="secondary"></v-checkbox>
                 </v-col>
 
                 <v-col cols="6" md="3" lg="2">
-                  <v-checkbox label="Remove duplicates" v-model="remove_duplicates" @change="joinText" color="secondary"></v-checkbox>
+                  <v-checkbox label="Remove duplicates" v-model="remove_duplicates" @change="joinText"
+                              color="secondary"></v-checkbox>
                 </v-col>
               </v-row>
 
@@ -107,7 +122,7 @@
 </template>
 
 <script>
-import { event } from 'vue-gtag'
+import {event} from 'vue-gtag'
 
 export default {
   data: () => ({
@@ -122,6 +137,8 @@ export default {
     remove_duplicates: true,
     each_delim_start: '',
     each_delim_end: '',
+    substr_start: 0,
+    substr_end: 0,
     snackbar: false,
     snackbar_text: 'Copied to clipboard',
     history: []
@@ -150,6 +167,10 @@ export default {
         });
 
         for (let i = 0; i < unique_input.length; i++) {
+          if (this.substr_start > 0 || this.substr_end > 0) {
+            unique_input[i] = unique_input[i].substring(this.substr_start, this.substr_end)
+          }
+
           unique_input[i] = this.each_delim_start ? this.each_delim_start + unique_input[i] : unique_input[i]
           unique_input[i] = this.each_delim_end ? unique_input[i] + this.each_delim_end : unique_input[i]
         }
